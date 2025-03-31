98805060a7668889dd35d297bbd62878581ceac5
Jacques2001 <jacques-qiu@hotmail.com>
2025-03-31 13:03:41 +0200

    split done

--- ft_split.c
+++ ft_split.c
@@ -1,2 +1,135 @@
 #include "libft.h"
 
+int count_word(char const *str, char c)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while(str[i])
+	{
+		if((str[i] != c && str[i - 1] == c) || (i == 0 && str[i] != c))
+			count++;
+		i++;
+	}
+	return count;
+}
+
+int count_letter(char const *str, char c, int x)
+{
+	int i;
+
+	i = 0;
+	while(str[x] != c && str[x])
+	{
+		x++;
+		i++;
+	}
+	return i;
+}
+
+void fill_split(char **split, char const *str, char c)
+{
+	int i;
+	int x;
+	int y;
+
+	i = 0;
+	x = 0;
+	y = 0;
+	while(str[y])
+	{
+		if((str[y] != c && str[y - 1] == c) || (y == 0 && str[y] != c))
+		{
+			while(str[y] != c && str[y])
+				split[i][x++] = str[y++];
+			split[i][x] = '\0';
+			i++;
+			x = 0;
+		}
+		else
+			y++;
+	}
+	split[i] = NULL;
+}
+
+char **ft_split(char const *s, char c)
+{
+	int i;
+	int x;
+	char **split;
+
+	i = 0;
+	x = 0;
+	split = malloc((count_word(s, c) + 1) * sizeof(char*));
+	if(split == NULL)
+		return NULL;
+	while(s[x])
+	{
+		if((s[x] != c && s[x - 1] == c) || (x == 0 && s[x] != c))
+		{
+			split[i] = malloc((count_letter(s, c, x) + 1) * sizeof(char));
+			if(split[i] == NULL)
+				return NULL;
+			i++;
+		}
+		x++;
+	}
+	fill_split(split, s, c);
+	split[i] = NULL;
+	return split;
+}
+
+void print_split(char **split)
+{
+  int i;
+  i = 0;
+  while(split[i])
+  {
+    printf("%s\n", split[i]);
+    i++;
+  }
+}
+
+/*int main()
+{
+	char str[] = "1 1 1  1 1  1  1 1 1 1 1  2  2  2 2 2 2";
+	char c = ' ';
+	//printf("%d\n", count_word(str, c));
+	print_split(ft_split(str, c));
+	return 0;
+}*/
+
+int main()
+{
+	char c = ' ';
+  char *array[] = {
+  ",Hello",
+  "Hello                    ",
+  "                hello              ",
+  "1 1 1 1 1 1 1  1 1  1 1 1 1  1 1  1 1 1 2",
+  "                                   !,,,,,,,,,,,",
+  "",
+  "Hello",
+  "Hello, World ! 42 ?",
+  "               Jacques  est  super fort   !!!! ",
+  NULL
+  };
+  for(int i = 0; array[i] != NULL;i++ )
+  {
+    printf("============= TEST %d =================\n",i);
+    printf("string: [%s]\n",array[i]);
+    
+    char **tab;
+    tab = ft_split(array[i], c);
+    printf("\nft_split:\n");
+		int k = 0;
+		while(tab[k] != NULL)
+		{
+			printf("tab[%d] = {%s}\n",k, tab[k]);
+			k++;
+		}
+  }
+  return 0;
+}
