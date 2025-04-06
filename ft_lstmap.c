235a817ec96e4cba1dfd56aa251c139fac6ac2d1
Jacques2001 <jacques-qiu@hotmail.com>
2025-04-06 14:24:59 +0200

    je peux passer a printf

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,28 +1,27 @@
 #include "libft.h"
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list *new_lst;
-	t_list *current;
-	t_list *node;
+{	
+	t_list *wagon;
+	t_list *plan;
+	t_list *la;
 
-	new_lst = NULL;
-	node = NULL;
+	plan = NULL;
 	while(lst != NULL)
 	{
-		node = malloc(sizeof(t_list));
-		if(node == NULL)
+		wagon = malloc(sizeof(t_list));
+		if(wagon == NULL)
 		{
-			del(new_lst);
+			del(plan);
 			return NULL;
 		}
-		node->content = f(lst->content);
-		node->next = NULL;
-		if(new_lst == NULL)
-			new_lst = node;
+		wagon->content = f(lst->content);
+		wagon->next = NULL;
+		if(plan == NULL)
+			plan = wagon;
 		else
-			current->next = node;
-		current = node;
+			la->next = wagon;
+		la = wagon;
 		lst = lst->next;
 	}
-	return new_lst;
+	return plan;
 }
